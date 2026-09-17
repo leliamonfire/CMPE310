c21977730a8e7818fff54cd297e7950f53a0738f
leliamonfire <125816469+leliamonfire@users.noreply.github.com>
2026-09-17 10:39:43 -0400

    continuing lab3

--- lab3/README.md
+++ lab3/README.md
@@ -1,3 +1,5 @@
 # Lab 3
 ## Compile Lab 3
 gcc -nostdlib -no-pie lab3.s -o lab3
+## Run
+./lab3
--- lab3/lab3.s
+++ lab3/lab3.s
@@ -1,34 +1,52 @@
 .section .data
-prompt: .asciz "Enter a string: \n"
-len = . - prompt
+prompt1: .asciz "Enter first string: \n"
+len1 = . - prompt1
+prompt2: .asciz "Enter second string: \n"
+len2 = . - prompt2
 
 .section .bss
-.comm input, 256
+.comm input1, 256
+.comm input2, 256
 
 .section .text
 .global _start
 
 _start:
-    mov $1,     %rax    # write
-    mov $1,     %rdi    # stdout
-    mov $prompt,   %rsi    # buf
-    mov $len,   %rdx    # len
+    mov $1,         %rax    # write
+    mov $1,         %rdi    # stdout
+    mov $prompt1,   %rsi    # buf
+    mov $len1,      %rdx    # len
     syscall
 
-    mov $0,     %rax
-    mov $0,     %rdi
-    mov $input, %rsi
+    mov $0,     %rax    # read
+    mov $0,     %rdi    # stdin
+    mov $input1, %rsi   # load first input
+    mov $256,   %rdx    # length 256 bytes
+    syscall
+
+    mov $1,     %rax        # write
+    mov $1,     %rdi        # stdout
+    mov $prompt2,   %rsi    # buf
+    mov $len2,   %rdx       # len
+    syscall
+
+    mov $0,     %rax    # read
+    mov $0,     %rdi    # stdin
+    mov $input2, %rsi   # load second input
+    mov $256,   %rdx    # legnth 256 bytes
+    syscall
+
+    mov $input1, %eax
+    mov $input2, %ebx
+
+    xorl %eax, %ebx
+
+    mov $1,     %rax
+    mov $1,     %rdi
+    mov (%ebx), %rsi
     mov $256,   %rdx
-    syscall
-
-    mov $1,     %rax    # write
-    mov $1,     %rdi    # stdout
-    mov $input,   %rsi    # buf
-    mov $256,   %rdx    # len
-    syscall
+    #syscall
 
     mov $60,    %rax    # exit
     mov $0,     %rdi    # status
     syscall
-
-    
